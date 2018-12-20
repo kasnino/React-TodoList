48629048e278d249927643abbb76b7620066a5a5
kasnino <kasn.emprendedor@gmail.com>
2018-12-20 14:31:35 -0500

    pepe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TodoItems from './TodoItems';
+import {Grid, Cell} from 'react-mdl';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props); 
+
+  
+        this.state ={
+            items: []
+        };
+
+/*aqui llamos a las funciones */
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+
+  }
+
+  addItem(e){
+        if (this._inputElement.value !== ""){
+          var newItem = {
+              text: this._inputElement.value,
+              key: Date.now()
+          };
+
+          this.setState((prevState) => {
+            return { 
+              items: prevState.items.concat(newItem) 
+            };
+          });
+
+        }
+
+        this._inputElement.value = "";
+        console.log(this.state.items);
+        e.preventDefault();
+
+  }
+
+  deleteItem(key){
+
+    console.log("aqui los keys: " + key);
+
+    var filterredItems = this.state.items.filter(function(item){
+        return(item.key !== key)
+
+    }); 
+
+  this.setState({
+      items: filterredItems
+
+  });
+
+
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+    <Grid className="grid_list" style={{marginLeft: '0'}}>
+    <Cell col={6}>
+       <div className="todolist_resumen">
+       <h3>Resumen del Reto</h3>
+       <p>Primer reto realizado con React y otras tecnologias.
+       Todo list, crea y elimina.
+       </p>
+       <ul>
+       <li><pre>React Js</pre></li>
+       <li><pre>Estructura de una App React Js</pre></li>
+       <li>react-mdl :  design</li>
+       <li>react-flip-move</li>
+       <li> HTML & Css (hover & transicions)</li>
+       <li> font awesome</li>
+        <a href="https://www.twitter.com/kasnino" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-twitter-square fa-3x" aria-hidden="true" />
           </a>
+              <a href="https://www.linkedin.com/in/kasnino/" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-linkedin-square fa-3x" aria-hidden="true" />
+          </a>
+
+          {/* Github */}
+          <a href="https://www.linkedin.com/in/kasnino/" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square fa-3x" aria-hidden="true" />
+          </a>
+          <br/>
+          <h5>Kristian Serrano</h5>
+       </ul>
+       </div>
+     </Cell>
+     <Cell col={6}>  
+      <div className="todolist_main">
+       
+     
+        <header className="App-header">
+          <h1>
+            Todo List
+          </h1>
+          <div className="contededor_dos">
+          <form onSubmit={this.addItem}>
+              <input ref={(a) => this._inputElement = a}
+                     placeholder="Comenta algo!" className="input_app"></input>
+              <button className="button_app" tipe="submit"> Add </button>
+          </form>
+           <div className="todo_listaitems">
+           <TodoItems entries={this.state.items}
+                      delete={this.deleteItem}
+
+            />
+           </div>
+       </div>
+
         </header>
+         
       </div>
+    </Cell>
+    </Grid> 
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+import * as serviceWorker from './serviceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
